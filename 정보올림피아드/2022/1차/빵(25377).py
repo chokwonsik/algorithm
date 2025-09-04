c9f9edd7a2802dca3f09182f15f4747319807945
Kwonsik_Cho <chokwonsik15@gmail.com>
2025-09-04 16:31:31 +0900

    feat: Add 정보올림피아드/2022/1차/빵(25377).py

---
+++ 정보올림피아드/2022/1차/빵(25377).py
@@ -0,0 +1,13 @@
+N = int(input())
+cnt = 0
+time_l = []
+for i in range(N):
+    time_to_bakery, bread_ready_time = list(map(int, input().split()))
+    if time_to_bakery <= bread_ready_time:
+        time_l.append(bread_ready_time)
+
+if time_l:
+    print(min(time_l))
+else:
+    print("-1")
+
