3a9ecc22a9228f127788e9dbd9b3d7412e0e8268
Kwonsik_Cho <chokwonsik15@gmail.com>
2026-03-02 14:00:00 +0900

    크림빵(28214).py

--- 정보올림피아드/2023/1차/크림빵(28214).py
+++ 정보올림피아드/2023/1차/크림빵(28214).py
@@ -1,11 +1,19 @@
 """
-
-문제 이름:
-출처:
-알고리즘 분류:
+https://www.acmicpc.net/problem/28214
+문제 이름: 크림빵
+출처: 정보올림피아드 2023 1차대회 초등부 1번
+알고리즘 분류: 구현
 """
+
 def solve1():
-    pass
+    N, K, P = map(int, input().split())
+    b = list(map(int, input().split()))
+
+    result = 0
+    for i in range(0, N * K, K):
+        if b[i:i + K].count(0) < P:
+            result += 1
+    print(result)
 
 
 if __name__ == "__main__":
